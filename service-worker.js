14d0919232168ef37a12498556804a0f2c4cc886
thomasjulien5-create <thomas.julien5@gmail.com>
2026-09-19 21:33:07 +0200

    v39: cache About artwork

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='ad-fontes-v38';
-const STATIC=['./','./index.html','./installer/','./installer/index.html','./styles.css?v=38','./app.js?v=38','./meditations.js?v=38','./assets/file_0000000000c082108ce827725e006074.png?v=38','./assets/home-bible.webp?v=38','./assets/home-thomas.webp?v=38','./assets/home-about.webp?v=38','./assets/bible-autel-v38.webp?v=38','./manifest.webmanifest','./icon.svg'];
+const CACHE='ad-fontes-v39';
+const STATIC=['./','./index.html','./installer/','./installer/index.html','./styles.css?v=39','./app.js?v=39','./meditations.js?v=39','./assets/file_0000000000c082108ce827725e006074.png?v=39','./assets/home-bible.webp?v=39','./assets/home-thomas.webp?v=39','./assets/home-about.webp?v=39','./assets/bible-autel-v38.webp?v=39','./assets/about-landscape-v39.webp?v=39','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
