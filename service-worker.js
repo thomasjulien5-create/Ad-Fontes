077cf2698e64ff0192049ed1479024eff6614322
thomasjulien5-create <thomas.julien5@gmail.com>
2026-09-19 21:34:32 +0200

    v40: cache Saint Thomas artwork

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='ad-fontes-v39';
-const STATIC=['./','./index.html','./installer/','./installer/index.html','./styles.css?v=39','./app.js?v=39','./meditations.js?v=39','./assets/file_0000000000c082108ce827725e006074.png?v=39','./assets/home-bible.webp?v=39','./assets/home-thomas.webp?v=39','./assets/home-about.webp?v=39','./assets/bible-autel-v38.webp?v=39','./assets/about-landscape-v39.webp?v=39','./manifest.webmanifest','./icon.svg'];
+const CACHE='ad-fontes-v40';
+const STATIC=['./','./index.html','./installer/','./installer/index.html','./styles.css?v=40','./app.js?v=40','./meditations.js?v=40','./assets/file_0000000000c082108ce827725e006074.png?v=40','./assets/home-bible.webp?v=40','./assets/home-thomas.webp?v=40','./assets/home-about.webp?v=40','./assets/bible-autel-v38.webp?v=40','./assets/about-landscape-v39.webp?v=40','./assets/thomas-meditation-v40.webp?v=40','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(STATIC)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{
